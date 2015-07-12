a9a431e500b1b888b13fe5df63c1b1a08fad210d
Reline <nathanielreline@gmail.com>
2015-07-12 15:04:40 -0700

    Implemented YouTube API (on Play - Tap Mode).

--- Asobou/app/src/main/java/fuwafuwa/asobou/PlayTapModeActivity.java
+++ Asobou/app/src/main/java/fuwafuwa/asobou/PlayTapModeActivity.java
@@ -6,12 +6,30 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class PlayTapModeActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class PlayTapModeActivity extends YouTubeFailureRecoveryActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_tap_mode);
+
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(DeveloperKey.DEVELOPER_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("wKJ9KzGQq0w");
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
     }
 
     @Override
