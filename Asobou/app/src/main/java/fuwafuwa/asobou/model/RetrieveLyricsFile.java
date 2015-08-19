0136fc839fd900fe7af3675823aa0598d3103f69
Reline <nathanielreline@gmail.com>
2015-08-19 04:56:26 -0700

    Fixed missing end character on each line for lyrics.

--- Asobou/app/src/main/java/fuwafuwa/asobou/model/RetrieveLyricsFile.java
+++ Asobou/app/src/main/java/fuwafuwa/asobou/model/RetrieveLyricsFile.java
@@ -42,12 +42,12 @@ public class RetrieveLyricsFile extends AsyncTask<String, String, String>
         for (String line:rawArray) {
             String startLyrics = line.substring(0,8);
             String endLyrics = line.substring(9,17);
-            String lyricLine = line.substring(18, line.length() - 1);
+            String lyricLine = line.substring(18, line.length());
 
             if(rawArray[0].equals(line)) { // invis chars ftw
                 startLyrics = line.substring(1,9);
                 endLyrics = line.substring(10,18);
-                lyricLine = line.substring(19, line.length() - 1);
+                lyricLine = line.substring(19, line.length());
             }
 
             lyrics.add(new LyricLine(lineNumber++, startLyrics, endLyrics, lyricLine));
